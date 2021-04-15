08a544418268393d88ba2ac6c9b15d311c852142
Gaku-Shimizu <gacky1280@gmail.com>
2021-04-15 11:15:33 +0900

    2021/04/15

--- W02/vec3.js
+++ W02/vec3.js
@@ -2,21 +2,58 @@ class Vec3
 {
     constructor(x, y, z)
     {
-	this.x = x;
-	this.y = y;
-	this.z = z;
+        this.x = x;
+	    this.y = y;
+	    this.z = z;
     }
 
     add(v)
     {
-	this.x += v.x;
-	this.y += v.y;
-	this.z += v.z;
-	return this;
+	    this.x += v.x;
+	    this.y += v.y;
+	    this.z += v.z;
+	    return this;
     }
 
-    sum(v)
+    sum()
     {
-	return this.x + this.y + this.z;
+	    return this.x + this.y + this.z;
+    }
+
+    min()
+    {
+        min = this.x;
+        if(min > this.y){
+            min = this.y;
+        }
+        if(min > this.z){
+            min = this.z;
+        }
+        return min;
+    }
+
+    max()
+    {
+        max = this.x;
+        if(max < this.y){
+            max = this.y;
+        }
+        if(max < this.z){
+            max = this.z;
+        }
+        return max;
+    }
+
+    mid()
+    {
+        if(this.x != this.min() && this.x != this.max()){
+            return this.x;
+        }
+        if(this.y != this.min() && this.y != this.max()){
+            return this.y;
+        }
+        if(this.z != this.min() && this.z != this.max()){
+            return this.z;
+        }
     }
 }
